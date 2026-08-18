2fa2f5ad10dbc99da56748ba8f3ed3f7b5d4128d
Aakash <113512885+tcgconsolee@users.noreply.github.com>
2026-08-18 23:32:09 +0800

    render

--- app.py
+++ app.py
@@ -308,4 +308,4 @@ def volunteer():
     return render_template('volunteer.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=10000)
